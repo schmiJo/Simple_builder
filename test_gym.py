6d651d59c3f1c5cbcc79fe55cf62fa2412d33836
Jonas Schmidt <schmidt.a.jonas@gmail.com>
2021-11-21 14:12:01 +0100

    added ppo base code

--- test_gym.py
+++ test_gym.py
@@ -1,7 +1,6 @@
 from gym_unity.envs import UnityToGymWrapper
 from mlagents_envs.environment import UnityEnvironment 
 import gym  
-from ddpg_agent import Agent
 from collections import deque
 
 unity_env = UnityEnvironment("./Builders_Sim.app")
@@ -14,14 +13,11 @@ print(env.observation_space.shape[0]);
 
 print(env.observation_space.shape[1]);
 
-agent = Agent(action_size=env.action_space.shape[0], state_size=7056)
 
 for _ in range(100000):
   env.render()
   action = env.action_space.sample() # your agent here (this takes random actions)
   observation, reward, done, info = env.step(action)
-  
-  
   
   if done:
     observation = env.reset()
